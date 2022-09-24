bcefb4a4287a2ac542349cc79e931a20c3fd4a73
David <cuentaporsi2@gmail.com>
2022-09-23 22:09:51 -0500

    xd

---
+++ XD.py
@@ -0,0 +1,23 @@
+import os
+
+# program to keep your desktop clean
+
+
+#print(dir(os))
+            
+                
+
+
+os.getcwd()
+#Use this to change dir
+os.chdir('New')
+#Use this to make dir    
+os.mkdir
+
+#TO make several dirs, exmple:
+os.makedirs('Folder/File')
+
+
+
+    
+
